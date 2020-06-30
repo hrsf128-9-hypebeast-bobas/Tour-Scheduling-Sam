856ce26fa14ca0563ec36b64e5480bbfb1e09799
Sam Sumsion <samsumsion729@gmail.com>
2020-06-30 15:29:45 -0700

    Fix DB

--- database/index.js
+++ database/index.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const connection = 'mongodb://localhost/tourschedule';
 
-const db = mongoose.connect(connection);
+const db = mongoose.connect(connection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = db;
